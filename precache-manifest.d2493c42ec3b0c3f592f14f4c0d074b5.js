184d98879c9287148b60033528ad1b6d8e4238df
Deadpool <deadpollkgz@gmail.com>
2020-03-22 00:50:52 +0600

    Updates

---
+++ precache-manifest.d2493c42ec3b0c3f592f14f4c0d074b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177a6fe955b43ff769252617b2be5c7e",
+    "url": "/sharp-test/index.html"
+  },
+  {
+    "revision": "7a3d80131e37347f43ef",
+    "url": "/sharp-test/static/css/main.49aea157.chunk.css"
+  },
+  {
+    "revision": "b9a6a7a706fc91bbb956",
+    "url": "/sharp-test/static/js/2.48fc64db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sharp-test/static/js/2.48fc64db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3d80131e37347f43ef",
+    "url": "/sharp-test/static/js/main.72a00610.chunk.js"
+  },
+  {
+    "revision": "00001ec518c840c67b32",
+    "url": "/sharp-test/static/js/runtime-main.93003fa5.js"
+  }
+]);
